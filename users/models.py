e68a625ba0ed4fa427dbc4e337b6e82c21e653fd
erkenraimbaev <erken.orsk.2015@gmail.com>
2024-04-03 22:35:32 +0500

    Создал модель user, описал представления и пути, настроил авторизацию через SimpleJWT

--- users/models.py
+++ users/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """
+    Модель для создания пользователей
+    """
+
+    ROLES = {
+        ('user', 'пользователь'),
+        ('admin', 'админ')
+    }
+
+    username = None
+    first_name = models.CharField(max_length=150, verbose_name='имя', **NULLABLE)
+    last_name = models.CharField(max_length=150, verbose_name='фамилия', **NULLABLE)
+    phone = models.CharField(max_length=50, verbose_name='номер телефона', **NULLABLE)
+    email = models.EmailField(unique=True, verbose_name='почта')
+    role = models.CharField(max_length=150, choices=ROLES, default='user', verbose_name='роль пользователя')
+    image = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email} {self.role}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer, MyTokenObtainPairSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_update(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserDeleteView(generics.DestroyAPIView):
+    queryset = User.objects.all()
